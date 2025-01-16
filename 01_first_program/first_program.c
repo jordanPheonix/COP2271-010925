1d74da4e91283d44226e9c66123c1c8811807b7a
jordanPheonix <jdexter9737@floridapoly.edu>
2025-01-16 16:32:02 +0000

    Created first C program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,21 @@
+//one line comment
+
+//This is the first program which prints a welcoming message
+
+/*
+
+Multiline comment.
+
+January 9th 2025.
+
+*/
+
+#include <stdio.h>
+
+//Create a main function
+int main() {
+    printf("Welcome to COP2271!\n");
+    printf("I want to code!\n"); // \n creates a new line; works the same in Python
+
+    return 0; //returning a value of zero means that the function executed successfully
+}
