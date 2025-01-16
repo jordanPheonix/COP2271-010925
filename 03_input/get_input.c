8fb3cbdb7468bdf6bbc800204be0b7d7daf8be3b
jordanPheonix <jdexter9737@floridapoly.edu>
2025-01-16 16:53:14 +0000

    Created Test Input Code

---
+++ 03_input/get_input.c
@@ -0,0 +1,26 @@
+/*
+
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+    int square_side = 1;
+    int square_perimeter = 0;
+    int square_area;
+
+    //Write the prompt
+    puts("Enter the side of a square.");
+
+    //get the input
+    scanf("%d", &square_side); //sanf("<conversion specifier(s)>", &<var_name>) || & - address modifier (looks for input from user keyboard)
+
+    //Do the math
+    square_perimeter = square_side * 4;
+
+    //Create the output
+    printf("The perimeter of a square with a side length of %d is %d.\n", square_side, square_perimeter);
+
+    return 0;
+}
