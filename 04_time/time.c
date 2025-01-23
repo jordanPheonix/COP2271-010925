8f79379d521021eb392387deebd0930e817dd005
jordanPheonix <jdexter9737@floridapoly.edu>
2025-01-23 16:13:21 +0000

    Met Instructor Request in time.c

--- 04_time/time.c
+++ 04_time/time.c
@@ -11,22 +11,31 @@ int main(void) {
     int seconds = 0;
     int remaining_seconds = 0;
     int minutes = 0;
+    int remaining_minutes = 0;
     int hours = 0;
+    int remaining_hours = 0;
+    int days = 0;
 
     //Get the number of seconds
     puts("Enter the number of seconds:");
     scanf("%d", &seconds);
 
-    //Calculate number of minutes
+    //Calculations
     minutes = seconds / 60;
+    hours = minutes / 60;
+    days = hours / 24;
 
-    //Calculate remaining seconds
+    //Calculate remaining seconds/minutes/hours
     remaining_seconds = seconds % 60; //same, but shorter than, "seconds = seconds - 60 * minutes"
+    remaining_minutes = minutes % 60;
+    remaining_hours = hours % 24;
 
     //Print result (with multi-line printf function)
-    printf("%d seconds is %d minutes and %d seconds\n", 
+    printf("%d seconds is %d days, %d hours, %d minutes, and %d seconds.\n", 
         seconds, 
-        minutes, 
+        days,
+        remaining_hours,
+        remaining_minutes, 
         remaining_seconds);
 
 
