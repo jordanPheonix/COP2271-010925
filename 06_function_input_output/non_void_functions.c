423ae683ad9adc7fe6aa9931d2521f75add27d2a
jordanPheonix <jdexter9737@floridapoly.edu>
2025-01-28 17:06:35 +0000

    end of class prt 2

--- 06_function_input_output/non_void_functions.c
+++ 06_function_input_output/non_void_functions.c
@@ -12,6 +12,8 @@ void printCourse(int course_number);
 
 void weather (int temperature);
 
+double milesToKms(int miles);
+
 int main(void){
     int var = 0;
     
@@ -22,6 +24,8 @@ int main(void){
     printCourse(2207);
 
     weather(45);
+
+    printf("%.4f\n", milesToKms(5)); //%<decimal places>f allows for printf to print a double
 
     return 0;
 }
@@ -43,4 +47,10 @@ void weather(int temperature){
     else {
         printf("The weather is cold\n");
     }    
+}
+
+double milesToKms(int miles){
+    double km = miles * 1.609;
+
+    return km;
 }
